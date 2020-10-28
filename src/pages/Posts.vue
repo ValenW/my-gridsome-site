21ebc8803a36291cb6c5160e9cf4634e554cf8b0
Valen <wang_wenl@worksap.co.jp>
2020-10-28 15:46:38 +0800

    implement page to display graphQL result

---
+++ src/pages/Posts.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        <li v-for="{ node } in $page.posts.edges" :key="node.id">
+          <g-link>{{ node.title }}</g-link>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        id
+        title
+        content
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: "Posts",
+  data() {
+    return {};
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
