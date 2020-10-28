4eec40842bb45b7108c40e0ba314c9987064d640
Valen <wang_wenl@worksap.co.jp>
2020-10-28 12:51:02 +0800

    init with custom page

---
+++ src/pages/MyPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><h1>My Page</h1></div>
+</template>
+
+<script>
+export default {
+  name: "MyPage",
+  data() {
+    return {};
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/templates/MyCodePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><h1>My Code Page</h1></div>
+</template>
+
+<script>
+export default {
+  name: "MyCodePage",
+  data() {
+    return {};
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
