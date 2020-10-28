4b1c7b933c6976c9c7a3dfe4f9ed82ba4348a30f
Valen <wang_wenl@worksap.co.jp>
2020-10-28 16:01:50 +0800

    add route for each node in posts page

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -4,7 +4,7 @@
       <h1>Posts</h1>
       <ul>
         <li v-for="{ node } in $page.posts.edges" :key="node.id">
-          <g-link>{{ node.title }}</g-link>
+          <g-link :to="`/post/${node.id}`">{{ node.title }}</g-link>
         </li>
       </ul>
     </div>
