405a0c1b4bd7ebf097ba36ae35ac410138486aa1
Valen <wang_wenl@worksap.co.jp>
2020-10-28 15:59:43 +0800

    add collection node template file and router

---
+++ src/templates/Post.vue
@@ -0,0 +1,38 @@
+<template>
+<Layout>
+  <div>
+    <h1>Post {{ $page.post.id }}</h1>
+    <h2>{{ $page.post.title }}</h2>
+    <p>{{ $page.post.content }}</p>
+  </div>
+</Layout>
+</template>
+
+<page-query>
+query($id: ID!) {
+  post(id: $id) {
+    id
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+ name: 'Post',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
